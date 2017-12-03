270655ebe1fd4abb8df57c4ff223d7dc57f7da63
Bryon Meinka <b.meinka@gmail.com>
2017-12-03 17:47:20 -0500

    add item weights

--- hunting/data.js
+++ hunting/data.js
@@ -1,13 +1,17 @@
 var data = {
-	item_types: ["encounter", "exotic", "rare", "uncommon", "common"],
 	base_rates: {
 		encounter: 0.05,
 		exotic: 0.02,
 		rare: 0.13,
 		uncommon: 0.20,
 	},
+	weight: {
+		companions: { encounter: [0, 3, 4] },
+		bags: { uncommon: [3], exotic: [1, 3] },
+		coins: { exotic: [0] },
+		shards: { exotic: [5] },
+	},
 	base_effects: {
-		// cub, double, beads, weights
 		item_count: { min: 1, max: 4 },
 	},
 	blessing: {
@@ -16,7 +20,7 @@ var data = {
 		mamota: { effect: "rare", value: 0.10 },
 	},
 	primal: {
-		queens_love: {},
+		queens_love: {}, // TODO cubs
 		night_fall: { effect: "rare", value: 0.05, requires: "night", companion: "include" },
 		sun_chaser: { effect: "extra_item", value: 0.10, companion: "multiply" },
 		snow_wanderer: { effect: "rare", value: 0.10, requires: "snow", companion: "multiply" },
@@ -24,25 +28,39 @@ var data = {
 		protector_of_the_forest: { effect: "rare", value: 0.10, requires: "forest", companion: "multiply" },
 	},
 	trait: {
-		accident_prone: {},
-		befriender: {},
-		fortunes_favor: {},
-		survivor: {},
+		accident_prone: { effect: "encounter", value: 0.05 },
+		befriender: { effect: "weight", value: 0.03, group: "companions" },
+		fortunes_favor: { effect: "beads", value: 0.05, min: 100, max: 600},
+		survivor: { effect: "exotic", value: 0.03 },
 		loner: { effect: "not_common", value: 0.10, companion: "negate" },
-		quick_learner: {},
-		treasure_hunter: {},
+		quick_learner: { effect: "weight", value: 0.05, group: "shards" },
+		treasure_hunter: { effect: "weight", value: 0.02, group: "coins" },
 		legendary_resilience: { effect: "not_common", value: 0.05 },
 	},
 	tack: {
 		claw_of_the_great_hunt: { effect: "not_common", value: 0.05 },
-		horn_of_beasts: {},
+		horn_of_beasts: { effect: "weight", value: 0.03, group: "companions" },
 		sharpened_spear: { effect: "not_common", value: 0.05 },
 		hide_wrapped_quiver: { effect: "not_common", value: 0.03 },
 		hyaenodon: { effect: "not_common", value: 0.05 },
-		dodo: {},
-		pygmy_onyc: {},
+		dodo: { effect: "weight", value: 0.02, group: "coins" },
+		pygmy_onyc: { effect: "weight", value: 0.05, group: "bags" },
 		umbrus_familiar: { effect: "extra_item", value: 1.00 },
-		spectral_trilobite: {},
+		spectral_trilobite: { effect: "weight", value: 0.05, group: "shards" },
 		black_cat: { effect: "extra_item", value: 1.00 },
 	},
 };
+var items = {
+	common: [ 692155725, 692155733, 696851410, 688815084, 696851245, 692155738, 695683108, 714394111, 714394107 ],
+	uncommon: [ 692155722, 692155730, 696852253, 696851734, 696851973, 692155740, 692155743 ],
+	rare: [ 688402076, 688811667, 688811665, 688811679, 688811676, 688818335, 695683103 ],
+	exotic: [ 688402080, 692155720, 695683106, 696852338, 696852465, 713651989 ],
+};
+var encounters = [
+	"Something scampers up to you. A tiny cub, lost and afraid refuses to leave your side. Maybe you could give it a home. (Companion Awarding)",
+	"Angry growls erupt from behind you as you're confronted by a fearsome lion. Her fur bristles and her muscles tense as she slowly, but defensively approaches. How do you react?",
+	"The hunter becomes the hunted. A massive sabertooth has been tracking you silently for miles, waiting for the right time to strike. Are you aware of the danger, or are you ambushed?",
+	"Tiny whines sound from behind a patch of tall grass. You peek behind and find a small surprise staring up at you, hungry and dangerously alone. It looks like its mother had been killed and the only one from its litter to survive. You take pity on the small hyaenodon and bring it home. (Companion Awarding)",
+	"As you stalk through the tall grass, tiny claws dig into your back and high pitched screech-squawks fill your ears. A tiny terror bird has decided to join your hunt, and also your pack it seems... (Companion Awarding)",
+];
+var output_template = ""; // TODO
--- hunting/hunting.js
+++ hunting/hunting.js
@@ -1,27 +1,31 @@
-function log_roll(what_for, chance) {
-	var chance_percent = Math.floor(chance * 100);
-	console.log("Rolling for " + what_for + " with a " + chance_percent + "% chance");
+function rng() {
+	return Math.random();
 }
 
-function rng() {
-	var n = Math.random();
-	console.log("Rolled " + Math.floor(n * 100));
-	return n;
+function rng_choice(options) {
+	var sum = 0;
+	options.forEach(function(option) { sum += option.chance; });
+	var factor = 1 / sum;
+	var n = 1.00;
+	var choice = rng();
+	for (var i = options.length - 1; i >= 0; --i) {
+		var v = options[i].chance * factor;
+		if (choice > (n - v))
+			return options[i].value;
+		n -= v;
+	}
+	console.log("ERROR: somehow we didn't return a valid choice!");
+	return options[0].value;
 }
 
 function get_num_items(player) {
 	var item_count = data.base_effects.item_count;
-	console.log("Rolling for item count");
 	var n = Math.floor(rng() * item_count.max) + item_count.min;
-	console.log("Got " + n + " items");
 	player.extra_item.forEach(function(item, index, arr) {
-		log_roll("an extra item", item);
 		if (rng() > (1.00 - item)) {
-			console.log("Got an extra item");
 			n++;
 		}
 	});
-	console.log("Giving " + n + " items!");
 	return n;
 }
 
@@ -30,28 +34,15 @@ function get_common_item(player) {
 	["encounter", "exotic", "rare", "uncommon", "not_common"].forEach(function(i) {
 		chance -= player.rates[i];
 	});
-	log_roll("a common item", chance);
 	return (rng() < chance)
 }
 
 function get_uncommon_item(player) {
-	var chosen_type = false;
-	var total = 0;
-	var types = ["encounter", "exotic", "rare", "uncommon"];
-	types.forEach(function(i) {
-		total += player.rates[i];
+	var options = [];
+	["uncommon", "rare", "exotic", "encounter"].forEach(function(n) {
+		options.push({ value: n, chance: player.rates[n] });
 	});
-	console.log("Rolling for rarer item type");
-	var r = rng() * total;
-	types.forEach(function(type) {
-		if (!chosen_type) {
-			if (r > (total - player.rates[type]))
-				chosen_type = type;
-			else
-				total -= player.rates[type];
-		}
-	});
-	return chosen_type;
+	return rng_choice(options);
 }
 
 function get_item_type(player) {
@@ -61,16 +52,28 @@ function get_item_type(player) {
 	else {
 		type = get_uncommon_item(player);
 	}
-	console.log("got a(n) " + type + " item");
 	return type;
+}
+
+function get_encounter(player) {
+	return encounters[Math.floor(rng() * encounters.length)];
+}
+
+function get_item(type, player) {
+	return items[type][Math.floor(rng() * items[type].length)];
 }
 
 function get_loot(player) {
 	var loot = {items: [], encounters: []};
 	var item_count = get_num_items(player);
 	for (var i = 0; i < item_count; ++i) {
-		loot.items.push(get_item_type(player));
+		var type = get_item_type(player);
+		if (type == "encounter")
+			loot.encounters.push(get_encounter(player));
+		else
+			loot.items.push(get_item(type, player));
 	}
+	// cubs, beads
 	return loot;
 }
 
@@ -112,6 +115,19 @@ function get_player_info() {
 	return info;
 }
 
+function add_weight(weights, effect, value) {
+	var group = data.weight[effect.group];
+	["common", "uncommon", "rare", "exotic", "encounter"].forEach(function(type) {
+		if (type in group) {
+			group[type].forEach(function(i) {
+				if (!weights[type][i])
+					weights[type][i] = 1.00;
+				weights[type][i] += value;
+			});
+		}
+	});
+}
+
 function get_player() {
 	var info = get_player_info();
 	var player = {
@@ -123,6 +139,13 @@ function get_player() {
 			uncommon: data.base_rates.uncommon,
 			not_common: 0.00
 		},
+		weights: {
+			common: [],
+			uncommon: [],
+			rare: [],
+			exotic: [],
+			encounter: [],
+		},
 		add_effect: function(effect) {
 			var v = info.value(effect);
 			if (effect.effect == "extra_item")
@@ -131,8 +154,25 @@ function get_player() {
 				this.rates.not_common += v;
 			if (effect.effect == "rare")
 				this.rates.rare += v;
+			if (effect.effect == "exotic")
+				this.rates.exotic += v;
+			if (effect.effect == "encounter")
+				this.rates.encounter += v;
+			if (effect.effect == "weight")
+				add_weight(this.weights, effect, v);
+			// weights
+			// beads
+			// double
+			// cubs
 		}
 	};
+
+	["common", "uncommon", "rare", "exotic"].forEach(function(type) {
+		for (var i = 0; i < items[type].length; ++i)
+			player.weights[type][i] = 1.00;
+	});
+	for (var i = 0; i < encounters.length; ++i)
+		player.weights.encounter[i] = 1.00;
 
 	["blessing", "primal", "trait", "tack"].forEach(function(name) {
 		info[name].forEach(function(item) {
@@ -148,7 +188,10 @@ function get_player() {
 function get_output(player, loot) {
 	var out = "";
 	loot.items.forEach(function(item) {
-		out += ":" + item + ":";
+		out += ":thumb" + item + ":";
+	});
+	loot.encounters.forEach(function(encounter) {
+		out += "\n\n" + encounter;
 	});
 	return out;
 }
